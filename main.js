ecc3f171ce7900661548ae4ccd73d9166d646628
emyllynayara <138024127+emyllynayara@users.noreply.github.com>
2023-10-11 11:28:07 -0300

    .....

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     var mostrarMensagemBtn = document.getElementById("mostrarMensagem");
 
     mostrarMensagemBtn.addEventListener("click", function () {
-        mensagem.innerHTML = "Queridos professores, hoje é o dia de celebrar vocês, " +
-        "os verdadeiros heróis que moldam mentes e inspiram sonhos. " +
+        mensagem.innerHTML = "Queridos professores, hoje é uma data especial que celebra e homenageia aqueles que têm o dom e a missão de inspirar e educar. " +
+        "Aqueles que compartilham conhecimentos e ensina a aprende " +
         "Em cada sala de aula, vocês desempenham um papel vital na formação do futuro. " +
-        "Seu comprometimento, paixão e dedicação são admiráveis e dignos de reconhecimento. " +
-        "Vocês não apenas compartilham conhecimento, mas também cultivam valores, orientam e apoiam os sonhos de seus alunos. " +
-        "Com paciência infinita, vocês transformam desafios em oportunidades de aprendizado e capacitam os jovens a serem cidadãos responsáveis e pensadores críticos. " +
-        "Neste Dia dos Professores, quero expressar minha mais profunda gratidão por tudo o que vocês fazem. " +
-        "Vocês são faróis de luz na jornada educacional de todos nós, iluminando o caminho com sabedoria e compreensão. " +
+        "Seu comprometimento, paixão e dedicação são admiráveis e dignos de reconhecimentoHoje e é hora de refletirmos sobre isso " +
+        "Com paciência infinita, vocês transformam desafios em oportunidades de aprendizado e capacitam os jovens a serem cidadãos responsáveis e pensadores " +
+        "Parabéns, professores, por esse dia, que Deus os abençoe sempre, e que o valor de vocês seja reconhecido! " +
+        "Graças aos professores sabemos ler e escrever " +
         "Obrigado por inspirarem, orientarem e ajudarem a construir um futuro melhor para todos. " +
-        "Feliz Dia dos Professores!";
+        "Feliz Dia dos Professores!♡ " +
     });
 });
 
